6746b1c4721f17b908ab100f1d3b4479caa100ab
Bruno-Tome <bruno.tome678@gmail.com>
2017-08-14 11:57:15 -0300

    Create Calc.c

---
+++ Calc.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include"calc.h"
+#include<math.h>
+
+int main(){
+
+char operacao;
+printf("Escolha a operacao desejada:\n a-Soma b-Subtracao c-Multiplicacao d-Divisao e-Raiz quadrada\n\n");
+
+scanf("%c", &operacao);
+double a, b, n;
+
+if(operacao == 'a'){
+printf("Digite dois numeros:  \n   ");
+scanf("%lf %lf", &a, &b);
+	printf("  Resultado:  %lf\n   ", Sum(a, b));
+}
+if(operacao == 'b'){
+printf("Digite dois numeros:  \n   ");
+scanf("%lf %lf", &a, &b);
+	printf("  Resultado:  %lf\n   ", subt(a, b));
+}
+if(operacao == 'd'){
+printf("Digite dois numeros:  \n   ");
+scanf("%lf %lf", &a, &b);
+	printf("  Resultado:  %lf\n   ", D(a, b));
+}
+if(operacao == 'c'){
+printf("Digite dois numeros:  \n   ");
+scanf("%lf %lf", &a, &b);
+	printf("  Resultado:  %lf\n   ", mult(a, b));
+}
+if(operacao == 'e'){
+printf("Digite um numero:  \n   ");
+scanf("%lf", &n);
+	printf("  Resultado:  %lf\n   ", raiz(n));
+}
+return 0;
+}
